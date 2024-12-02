6ab0028726b3ae2a21fad79d9ecb81b163f212c1
StartZhao <143171493+StartZhao@users.noreply.github.com>
2024-12-02 12:49:02 +0800

    更新背景加载慢问题

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -3,43 +3,38 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount,computed } from 'vue';
+import { ref, onMounted, computed } from "vue";
 
 // 定义背景图像数组
-const backgrounds = [
-  'images/background1.jpg',
-  'images/background2.jpg',
-];
+const backgrounds = ["images/background1.jpg", "images/background2.jpg"];
 
 const currentIndex = ref(0);
 const backgroundUrl = ref(backgrounds[currentIndex.value]);
 
 // 添加时间戳以防止缓存，使得背景更换能够识别到
 const backgroundStyle = computed(() => ({
-  backgroundImage: `url(${backgroundUrl.value}?${new Date().getTime()})`, // 避免缓存
+  backgroundImage: `url(${backgroundUrl.value})`, // 避免缓存
 }));
 
+// 获取随机索引
+function getRandomIndex() {
+  return Math.floor(Math.random() * backgrounds.length);
+}
 
-const changeBackground = () => {
-  console.log('New background URL:', backgroundUrl.value);
-  currentIndex.value = (currentIndex.value + 1) % backgrounds.length; // 循环切换
-  backgroundUrl.value = backgrounds[currentIndex.value];
+// 预加载图片
+const preloadImages = (start: number, end: number): void => {
+  for (let i = start; i < end && i < backgrounds.length; i++) {
+    const img = new Image();
+    img.src = backgrounds[i];
+  }
 };
 
-// 定时切换背景
-let intervalId: number;
-
 onMounted(() => {
-  intervalId = window.setInterval(changeBackground, 60*60*1000); // 每1小时切换
+  // 初始化时设置随机背景
+  backgroundUrl.value = backgrounds[getRandomIndex()];
+  // 延迟几秒后再预加载其他图片
+  setTimeout(preloadImages, 5000);
 });
-
-onBeforeUnmount(() => {
-  if(intervalId){
-    clearInterval(intervalId); // 清理定时器
-  }
-});
-
-
 </script>
 
 <style scoped>
